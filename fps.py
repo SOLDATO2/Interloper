c913b50a17becaf376d1363f7d656aeafdddf8c2
Fe <felipeporto553@gmail.com>
2024-10-20 14:59:14 -0300

    v3

--- fps.py
+++ fps.py
@@ -1,18 +1,19 @@
 import cv2
 
-# Caminho para o vídeo MP4
-video_path = 'interlope_videos\\darkurge_talk_interlope.mp4'
+def calcular_fps(video_path):
+    # Abrir o vídeo
+    cap = cv2.VideoCapture(video_path)
 
-# Abre o vídeo
-cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        print("Erro ao abrir o vídeo.")
+        return
 
-# Verifica se o vídeo foi aberto corretamente
-if not cap.isOpened():
-    print("Erro ao abrir o vídeo.")
-else:
-    # Obtém a quantidade de FPS do vídeo
+    # Obter a taxa de quadros (FPS)
     fps = cap.get(cv2.CAP_PROP_FPS)
-    print(f"Quantidade de FPS: {fps}")
+    cap.release()
+    return fps
 
-# Libera o objeto de captura de vídeo
-cap.release()
+# Exemplo de uso
+video_path = 'videos_30fps\\gta5.mp4'
+fps = calcular_fps(video_path)
+print(f'A quantidade de FPS do vídeo é: {fps}')
